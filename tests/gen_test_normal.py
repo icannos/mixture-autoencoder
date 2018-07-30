b0289f491a8bce4e14a27768ce702162af34f467
Icannos <maxime.darrin@ens-lyon.fr>
2018-07-30 15:45:06 +0200

    add tests gens

---
+++ tests/gen_test_normal.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.io import savemat
+
+
+centers = np.random.uniform(-1,1, (10, 8))
+
+clusters = []
+for c in centers:
+    clusters.append(np.random.normal(c, 0.3, 1000))
+
+clusters = np.asarray(clusters)
+
+savemat("clusters_norm_10_train", {"X": clusters})
+
+for i,c in enumerate(centers):
+    savemat("clusters_norm_10_test_" + str(i), {"X":np.random.normal(c, 0.3, 1000)})

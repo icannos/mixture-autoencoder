4f65c8c734d7c59ebcbd90e9155a8e140ab79e8f
Icannos <maxime.darrin@ens-lyon.fr>
2018-07-30 15:52:02 +0200

    fix test gen

--- tests/gen_test_normal.py
+++ tests/gen_test_normal.py
@@ -6,11 +6,11 @@ centers = np.random.uniform(-1,1, (10, 8))
 
 clusters = []
 for c in centers:
-    clusters.append(np.random.normal(c, 0.3, 1000))
+    clusters.append(np.random.normal(c, 0.3, (1000, 8)))
 
 clusters = np.asarray(clusters)
 
-savemat("clusters_norm_10_train.mat", {"X": clusters})
+savemat("clusters_norm_10_train.mat", {"X": np.concatenate(clusters, axis=0)})
 
 for i,c in enumerate(centers):
-    savemat("clusters_norm_10_test_" + str(i) +".mat", {"X":np.random.normal(c, 0.3, 1000)})
+    savemat("clusters_norm_10_test_" + str(i) +".mat", {"X":np.random.normal(c, 0.3, (1000, 8))})
